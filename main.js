21d210abefdb4e3332ee0785a0dd6675edb5a843
nirmalpaul383 <40243913+nirmalpaul383@users.noreply.github.com>
2020-07-09 17:21:43 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+/** This project is originally made by me(N Paul).
+ My github profile https://github.com/nirmalpaul383/
+ My youtube page https://www.youtube.com/channel/UCY6JY8bTlR7hZEvhy6Pldxg/
+ This is an open source program . You are welcomed to modify it...
+ If you want to support me please give a like to our facebook page 
+ https://facebook.com/a.new.way.Technical/  **/
+
+var code = document.getElementById('code');
+var dis = document.getElementById('dis');
+var obj = [];
+var process;
+dis.value = `Welcome to JavaScript Console Mini by N Paul... Current Version 1.0.0
+
+`;
+
+
+function ok() {
+  dis.value = `${dis.value} User: ${code.value}
+`;
+  
+  
+  var i = 0;
+  obj.push(code.value);
+  console.log(obj.lenght);
+
+  while (i < obj.length ) {
+    if (i != (obj.length - 1))
+    {
+      eval(obj[i]);
+    }
+    else
+    {
+      process = eval(obj[i]);
+    };
+    i++;
+  };
+  dis.value = `${dis.value} JavaScript Engine: ${process}
+
+`;
+  code.value = "";
+}
